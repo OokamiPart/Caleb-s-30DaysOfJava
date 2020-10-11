d7dcc93097fa11d12f69a2fbcb83c18fc6e6c20a
Ookami Part <ookamipart01@gmail.com>
2020-10-11 13:50:08 +0700

    Modify Main.java .
    change txt file reader program to Scanner version

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.util.Scanner;
+import java.io.File;
 
 public class Main {
 	public static void main (String [] args) throws Exception{
-		BufferedReader read = new BufferedReader(new FileReader("name.txt"));
-		String name;
-		while((name = read.readLine()) != null ) {
-			System.out.println(name);
+		Scanner read = new Scanner(new File("name.txt"));
+		while(read.hasNextLine()) {
+			System.out.println(read.nextLine());
 		}
-		read.close();
 	}
 }
