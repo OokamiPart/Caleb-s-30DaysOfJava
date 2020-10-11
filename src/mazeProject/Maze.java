f31480b00547ebfd93bec3df81e8788759388456
Ookami Part <ookamipart01@gmail.com>
2020-10-11 21:01:27 +0700

    Maze project begin.
    Add Position.java.
        Add x (int).
        Add y (int).
    Update Maze.java.
        Add map variable (2D int array).
        Add path variable (LinkedList<Position>).
        Add current position variable (Position)

--- src/mazeProject/Maze.java
+++ src/mazeProject/Maze.java
@@ -1,10 +1,22 @@
 package mazeProject;
 
+import java.util.LinkedList;
+
 public class Maze {
+	
+	static int[][] map = {
+			{1, 0, 1, 1},
+			{1, 0, 0, 1},
+			{1, 1, 0, 1},
+			{0, 1, 1, 1}
+	};
+	
+	static LinkedList<Position> path = new LinkedList<Position>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Position currentPos = new Position(0, 0);
+		path.push(currentPos);
+		
+		map[path.peek().y][path.peek().x] = 0;
 	}
-
 }
---
+++ src/mazeProject/Position.java
@@ -0,0 +1,10 @@
+package mazeProject;
+
+public class Position {
+	public int x;
+	public int y;
+	public Position (int y, int x) {
+		this.x = x;
+		this.y = y;
+	}
+}
