e551c97c70d01172c023c224d1a8b9249b45d2d2
Ookami Part <ookamipart01@gmail.com>
2020-10-11 18:07:35 +0700

    Modify Main.java .
    Simplify FIFO & FILO program.
        delete size variable
        delete if condition (that determines the size)
        delete iterator interfaces.
        change while loops to for each loops.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.io.File;
@@ -9,7 +8,6 @@ public class Main {
 		
 		LinkedList<String> FIFO = new LinkedList<String>();
 		LinkedList<String> FILO = new LinkedList<String>();
-		int size;
 		
 		while(scan.hasNext()){
 			String name = scan.nextLine();
@@ -17,22 +15,13 @@ public class Main {
 			FILO.push(name);
 			System.out.println(name + " added");
 		}
-		
-		size = FIFO.size();
-		
-		if(FIFO.size() <= 0) size = FILO.size();
-		
-		Iterator<String> iteratorFIFO = FIFO.iterator();
-		Iterator<String> iteratorFILO = FILO.iterator();
-		
 		System.out.println("=== FIFO OUTPUT ===");
-		while(iteratorFIFO.hasNext()) {
-			System.out.println(iteratorFIFO.next());
+		for(String s : FIFO) {
+			System.out.println(s);
 		}
 		System.out.println("=== FILO OUTPUT ===");
-		while(iteratorFILO.hasNext()) {
-			System.out.println(iteratorFILO.next());
+		for(String s : FILO) {
+			System.out.println(s);
 		}
-		
 	}
 }
