cbb13d1623fea89ad82f2132dc571356dda61306
Ookami Part <ookamipart01@gmail.com>
2020-10-11 13:00:10 +0700

    Modify Main.java .
    Delete the random output program.
    Add Array program.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main (String [] args){
-		System.out.println("Dame dane, Dame yoo, Dame Nano yooo~~");
+		Scanner scan = new Scanner(System.in);
+		int size;
+		System.out.println("Determine the Array Size : ");
+		size = scan.nextInt();
+		
+		int[] arrayValue = new int[size];
+		
+		for(int i = 0; i < size; i++) {
+			int value;
+			System.out.println("Input the value : ");
+			value = scan.nextInt();
+			arrayValue[i] = value;
+		}
+		
+		for(int i = 0; i < size; i++) {
+			System.out.println("value of Array index " + i + " is "+ arrayValue[i]);
+		}
+		scan.close();
 	}
 }
