1f912c77501328c7f262927278847ff5d9e769f1
Ookami Part <ookamipart01@gmail.com>
2020-10-11 17:10:15 +0700

    Modify Main.Java .
    Delete txt file reader program.
    Add FIFO and FILO program.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,34 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.io.File;
 
 public class Main {
 	public static void main (String [] args) throws Exception{
-		Scanner read = new Scanner(new File("name.txt"));
-		while(read.hasNextLine()) {
-			System.out.println(read.nextLine());
+		Scanner scan = new Scanner(new File("name.txt"));
+		
+		LinkedList<String> FIFO = new LinkedList();
+		LinkedList<String> FILO = new LinkedList();
+		int size;
+		
+		while(scan.hasNext()){
+			String name = scan.nextLine();
+			FIFO.add(name);
+			FILO.push(name);
+			System.out.println(name + " added");
 		}
+		
+		size = FIFO.size();
+		
+		if(FIFO.size() <= 0) size = FILO.size();
+		
+		System.out.println("=== FIFO OUTPUT ===");
+		for(int i = 0; i < size; i++) {
+			System.out.println(FIFO.remove());
+		}
+		System.out.println("=== FILO OUTPUT ===");
+		for(int i = 0; i < size; i++) {
+			System.out.println(FILO.remove());
+		}
+		
 	}
 }
