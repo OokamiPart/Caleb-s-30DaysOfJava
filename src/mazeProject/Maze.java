f162bcf606c2cdc84ae2d9ac04408ee27b376e9c
Ookami Part <ookamipart01@gmail.com>
2020-10-12 08:09:15 +0700

    Modify Maze.java
    Increase the map
    Add down movement

--- src/mazeProject/Maze.java
+++ src/mazeProject/Maze.java
@@ -5,18 +5,35 @@ import java.util.LinkedList;
 public class Maze {
 	
 	static int[][] map = {
-			{1, 0, 1, 1},
-			{1, 0, 0, 1},
-			{1, 1, 0, 1},
-			{0, 1, 1, 1}
+			{0, 0, 0, 0, 0, 0},
+			{0, 1, 0, 2, 1, 0},
+			{0, 1, 0, 0, 1, 0},
+			{0, 1, 0, 0, 1, 0},
+			{0, 1, 1, 1, 1, 0},
+			{0, 0, 0, 0, 0, 0},
 	};
 	
 	static LinkedList<Position> path = new LinkedList<Position>();
 
 	public static void main(String[] args) {
-		Position currentPos = new Position(0, 0);
+		Position currentPos = new Position(1, 1);
 		path.push(currentPos);
-		
-		map[path.peek().y][path.peek().x] = 0;
+		while(true) {
+			int x = path.peek().x;
+			int y = path.peek().y;
+			map[y][x] = 0;
+			//Move Down
+			if (map[y+1][x] == 2) {
+				System.out.println("Moved down, found it!");
+				return;
+			} else if (map[y+1][x] == 1) {
+				System.out.println("Moved down");
+				path.push(new Position(y+1, x));
+				continue;
+			} else if (map[y+1][x] == 0) {
+				System.out.println("Blocked, Cant Move down");
+				return;
+			}
+		}
 	}
 }
